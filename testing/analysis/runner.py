c8f18ad9441cb3d2d655fc2c02a3aa46e65d2068
Bálint Kollmann <B.Kollmann@student.tudelft.nl>
2026-06-01 22:19:58 +0200

    analyse data

--- testing/analysis/runner.py
+++ testing/analysis/runner.py
@@ -7,30 +7,36 @@ TRACE_DIR = os.path.join(BASE_DIR, "data", "generated", "abbadingo", "traces")
 QUERY_DIR = os.path.join(BASE_DIR, "data", "generated", "abbadingo", "queries")
 RESULT_DIR = os.path.join(BASE_DIR, "data", "results", "abbadingo")
 
-FILENAMES = ["trainA", "trainB", "trainC", "trainD", "train.1", "train.2", "train.3", "trainR", "train.4", "train.5", "train.6", "trainS", "train.7", "train.8", "train.9", "trainT"]
+#FILENAMES = ["trainA", "trainB", "trainC", "trainD", "train.1", "train.2", "train.3", "trainR", "train.4", "train.5", "train.6", "trainS", "train.7", "train.8", "train.9", "trainT"]
 #FILENAMES = ["small_test"]
 #FILENAMES = ["trainA"]
+
+FILENAMES = ["trainA", "trainB", "train.1", "train.2",  "train.4", "train.5", "train.6", "train.7", "train.8", "train.9", "trainT"]
 
 FILE_PATHS_TRACES = [os.path.join(TRACE_DIR, fname + ".txt") for fname in FILENAMES]
 FILE_PATHS_QUERIES = [os.path.join(QUERY_DIR, fname + ".txt") for fname in FILENAMES]
 FILE_PATH_CSV = os.path.join(RESULT_DIR, "abbadingo_results.csv")
 EXE = os.path.join(BASE_DIR, "cpp", "build", "Release", "main_app.exe")
 
-#IMPLS = ["naive", "precomp", "sqrt"]
-IMPLS = ["sqrt"]
-REPEAT = 1
+IMPLS = ["naive", "precomp", "sqrt"]
+#IMPLS = ["sqrt"]
+REPEAT = 4
 
 def run_cpp(impl, inp):
-    result = subprocess.run(
-        [EXE, impl],
-        input=inp,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        text=True,
-        timeout=600
-    )
+    try:
+        result = subprocess.run(
+            [EXE, impl],
+            input=inp,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True,
+            timeout=200
+        )
 
-    return result.stdout.strip().split(",")
+        return result.stdout.strip().split(",")
+
+    except subprocess.TimeoutExpired:
+        return None
 
 #time is measured in microseconds (us)
 #memory is measured in bytes
@@ -51,22 +57,30 @@ def run_tests(dataset_id, inp_traces, inp_queries, outp):
 
     with open(outp, "a", newline="") as f:
         writer = csv.writer(f)
-        writer.writerow([
-            "dataset",
-            "impl",
-            "positive_set_size",
-            "negative_set_size",
-            "number_of_queries",
-            "build_time_us",
-            "query_time_us",
-            "memory_bytes",
-            "total_output_len"
-        ])
 
         for _ in range(REPEAT):
             for impl in IMPLS:
                 row = run_cpp(impl, inp)
-                writer.writerow([dataset_id] + row)
+                if row is None:
+                    writer.writerow([dataset_id, "TIMEOUT"] + [""] * 8)
+                else:
+                    writer.writerow([dataset_id, "OK"] + row)
+
+
+with open(FILE_PATH_CSV, "w", newline="") as f:
+    writer = csv.writer(f)
+    writer.writerow([
+        "dataset",
+        "status",
+        "impl",
+        "positive_set_size",
+        "negative_set_size",
+        "number_of_queries",
+        "build_time_us",
+        "query_time_us",
+        "memory_bytes",
+        "total_output_len"
+    ])
 
 for i in range(len(FILENAMES)):
     dataset = FILENAMES[i]
